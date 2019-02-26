a6d105b2dce9ef345ee0238de552df6fc6795105
zhao wen qiang <13143410521@163.com>
2019-02-26 14:32:50 +0800

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -3,5 +3,13 @@ describe('indx.js', () => {
     it('data.json', async () => {
         const data = require('../index')
         expect(data).to.be.an('array')
+        for (const iterator of data) {
+            expect(iterator).to.be.an('object')
+            expect(iterator).to.have.property('english')
+            expect(iterator).to.have.property('sexy')
+            expect(iterator).to.have.property('phonetic_symbol')
+            expect(iterator).to.have.property('chinese')
+            expect(iterator).to.have.property('source')
+        }
     });
 });
